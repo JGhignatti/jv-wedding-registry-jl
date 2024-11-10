1a2ca2e90adcbd2bda30f7422a5b63e1ddd4930b
JGhignatti <ghignatti.joao@gmail.com>
2024-11-10 19:56:22 -0300

    create editorconfig and setup prettier then format all root files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {},
+  },
 
-	plugins: [forms, containerQueries]
+  plugins: [forms, containerQueries],
 } satisfies Config;
